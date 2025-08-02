8e9d3a0ac2279c35da3715f20a5388cbe5c4786b
norteakorn <norteakorn@gmail.com>
2025-08-02 11:47:48 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,32 @@
+const cards = {
+  1: {
+    img: "images/Territorio1.png",
+    text: "Información del territorio 1"
+  },
+  2: {
+    img: "images/Territorio2.png",
+    text: "Información del territorio 2"
+  },
+  3: {
+    img: "images/Territorio3.png",
+    text: "Información del territorio 3"
+  }
+};
+
+function showCard(id) {
+  const container = document.getElementById("card-container");
+  const cardData = cards[id];
+
+  container.innerHTML = `
+    <div class="card" onclick="this.classList.toggle('flipped')">
+      <div class="card-inner">
+        <div class="card-front">
+          <img src="${cardData.img}" alt="Territorio ${id}" />
+        </div>
+        <div class="card-back">
+          ${cardData.text}
+        </div>
+      </div>
+    </div>
+  `;
+}
