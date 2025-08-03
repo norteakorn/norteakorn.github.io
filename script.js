8185a4d60aa0b60555d307ab16848049503a19e1
norteakorn <norteakorn@gmail.com>
2025-08-03 12:06:22 -0300

    Update script.js

--- script.js
+++ script.js
@@ -14,7 +14,220 @@ const cards = {
   4: {
     img: "images/Territorio 4.png",
     text: "Información del territorio 4"
-  }
+  },
+  5: {
+    img: "images/Territorio 5.png",
+    text: "Información del territorio 4"
+  },
+6: {
+    img: "images/Territorio 6.png",
+    text: "Información del territorio 4"
+  },
+7: {
+    img: "images/Territorio 7.png",
+    text: "Información del territorio 4"
+  },
+8: {
+    img: "images/Territorio 8.png",
+    text: "Información del territorio 4"
+  },
+9: {
+    img: "images/Territorio 9.png",
+    text: "Información del territorio 4"
+  },
+10: {
+    img: "images/Territorio 10.png",
+    text: "Información del territorio 4"
+  },
+11: {
+    img: "images/Territorio 11.png",
+    text: "Información del territorio 4"
+  },
+12: {
+    img: "images/Territorio 12.png",
+    text: "Información del territorio 4"
+  },
+13: {
+    img: "images/Territorio 13.png",
+    text: "Información del territorio 4"
+  },
+14: {
+    img: "images/Territorio 14.png",
+    text: "Información del territorio 4"
+  },
+15: {
+    img: "images/Territorio 15.png",
+    text: "Información del territorio 4"
+  },
+16: {
+    img: "images/Territorio 16.png",
+    text: "Información del territorio 4"
+  },
+17: {
+    img: "images/Territorio 17.png",
+    text: "Información del territorio 4"
+  },
+18: {
+    img: "images/Territorio 18.png",
+    text: "Información del territorio 4"
+  },
+19: {
+    img: "images/Territorio 19.png",
+    text: "Información del territorio 4"
+  },
+20: {
+    img: "images/Territorio 20.png",
+    text: "Información del territorio 4"
+  }.
+21: {
+    img: "images/Territorio 21.png",
+    text: "Información del territorio 4"
+  },
+22: {
+    img: "images/Territorio 22.png",
+    text: "Información del territorio 4"
+  },
+23: {
+    img: "images/Territorio 23.png",
+    text: "Información del territorio 4"
+  },
+24: {
+    img: "images/Territorio 24.png",
+    text: "Información del territorio 4"
+  },
+25: {
+    img: "images/Territorio 25.png",
+    text: "Información del territorio 4"
+  },
+26: {
+    img: "images/Territorio 26.png",
+    text: "Información del territorio 4"
+  },
+27: {
+    img: "images/Territorio 27.png",
+    text: "Información del territorio 4"
+  },
+28: {
+    img: "images/Territorio 28.png",
+    text: "Información del territorio 4"
+  },
+29: {
+    img: "images/Territorio 29.png",
+    text: "Información del territorio 4"
+  },
+30: {
+    img: "images/Territorio 30.png",
+    text: "Información del territorio 4"
+  },
+31: {
+    img: "images/Territorio 31.png",
+    text: "Información del territorio 4"
+  },
+32: {
+    img: "images/Territorio 32.png",
+    text: "Información del territorio 4"
+  },
+33: {
+    img: "images/Territorio 33.png",
+    text: "Información del territorio 4"
+  },
+34: {
+    img: "images/Territorio 34.png",
+    text: "Información del territorio 4"
+  },
+35: {
+    img: "images/Territorio 35.png",
+    text: "Información del territorio 4"
+  },
+36: {
+    img: "images/Territorio 36.png",
+    text: "Información del territorio 4"
+  },
+37: {
+    img: "images/Territorio 37.png",
+    text: "Información del territorio 4"
+  },
+38: {
+    img: "images/Territorio 38.png",
+    text: "Información del territorio 4"
+  },
+39: {
+    img: "images/Territorio 39.png",
+    text: "Información del territorio 4"
+  },
+40: {
+    img: "images/Territorio 40.png",
+    text: "Información del territorio 4"
+  },
+41: {
+    img: "images/Territorio 41.png",
+    text: "Información del territorio 4"
+  },
+42: {
+    img: "images/Territorio 42.png",
+    text: "Información del territorio 4"
+  },
+43: {
+    img: "images/Territorio 43.png",
+    text: "Información del territorio 4"
+  },
+44: {
+    img: "images/Territorio 44.png",
+    text: "Información del territorio 4"
+  },
+45: {
+    img: "images/Territorio 45.png",
+    text: "Información del territorio 4"
+  },
+46: {
+    img: "images/Territorio 4.png",
+    text: "Información del territorio 4"
+  },
+47: {
+  img: "images/Territorio 47.png",
+  text: "Información del territorio 47"
+},
+48: {
+  img: "images/Territorio 48.png",
+  text: "Información del territorio 48"
+},
+49: {
+  img: "images/Territorio 49.png",
+  text: "Información del territorio 49"
+},
+50: {
+  img: "images/Territorio 50.png",
+  text: "Información del territorio 50"
+},
+51: {
+  img: "images/Territorio 51.png",
+  text: "Información del territorio 51"
+},
+52: {
+  img: "images/Territorio 52.png",
+  text: "Información del territorio 52"
+},
+53: {
+  img: "images/Territorio 53.png",
+  text: "Información del territorio 53"
+},
+54: {
+  img: "images/Territorio 54 (RURAL).png",
+  text: "Información del territorio 54"
+},
+55: {
+  img: "images/Territorio 55 (RURAL).png",
+  text: "Información del territorio 55"
+},
+56: {
+  img: "images/Territorio 56 (RURAL).png",
+  text: "Información del territorio 56"
+},
+57: {
+  img: "images/Territorio 57 (RURAL).png",
+  text: "Información del territorio 57"
+}
+
   // agregá más tarjetas acá
 };
 
