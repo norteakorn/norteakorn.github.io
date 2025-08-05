b6077b3cb056771d7450edd2f751a2f4971cbe80
norteakorn <norteakorn@gmail.com>
2025-08-05 10:01:34 -0300

    Update script.js

--- script.js
+++ script.js
@@ -91,63 +91,165 @@ const cards = {
   },
 10: {
     img: "images/Territorio 10.png",
-    text: "Información del territorio 4"
+    text: `
+    <div style="text-align: left;">
+      <strong>No visitar</strong><br><br>
+
+      Velez Sarfield 53 <br>
+      28-11-18<br><br>
+
+      Ameghino 92 <br>
+     29-11-18<br><br
+
+     Almirante Brown 73  <br>
+     14-07-19<br><br>
+    </div>
+  `
   },
 11: {
     img: "images/Territorio 11.png",
-    text: "Información del territorio 4"
+    text: `
+    <div style="text-align: left;">
+      <strong>No visitar</strong><br><br>
+
+      San Martín 93  <br>
+      Diciembre 2015<br><br>
+
+      Bullrich 133  <br>
+     Diciembre 2015
+
+     El Salvador 969   <br>
+     Diciembre 2024<br><br>
+    </div>
+  `
   },
 12: {
     img: "images/Territorio 12.png",
-    text: "Información del territorio 4"
+    text: `
+    <div style="text-align: left;">
+      <strong>No visitar</strong><br><br>
+
+      Libertad 277 <br>
+      Diciembre 2015<br><br>
+
+      Uruguay 762 <br>
+      Enero 2015<br><br>
+    </div>
+  `
   },
 13: {
     img: "images/Territorio 13.png",
-    text: "Información del territorio 4"
+    text: `
+    <div style="text-align: left;">
+      <strong>No visitar</strong><br><br>
+
+      Esmeralda 76  <br>
+      09-12-18<br><br>
+
+      Esmeralda 62  <br>
+      09-12-18<br><br>
+
+      Suipacha 133<br>
+
+      Esmeralda 74<br>
+    </div>
+  `
   },
 14: {
     img: "images/Territorio 14.png",
-    text: "Información del territorio 4"
+    text: `
+    <div style="text-align: left;">
+      <strong>No visitar</strong><br><br>
+    </div>
+  `
   },
 15: {
     img: "images/Territorio 15.png",
-    text: "Información del territorio 4"
+    text: `
+    <div style="text-align: left;">
+      <strong>No visitar</strong><br><br>
+    </div>
+  `
   },
 16: {
     img: "images/Territorio 16.png",
-    text: "Información del territorio 4"
+    text: `
+    <div style="text-align: left;">
+      <strong>No visitar</strong><br><br>
+    </div>
+  `
   },
 17: {
     img: "images/Territorio 17.png",
-    text: "Información del territorio 4"
+    text: `
+    <div style="text-align: left;">
+      <strong>No visitar</strong><br><br>
+    </div>
+  `
   },
 18: {
     img: "images/Territorio 18.png",
-    text: "Información del territorio 4"
+    text: `
+    <div style="text-align: left;">
+      <strong>No visitar</strong><br><br>
+    </div>
+  `
   },
 19: {
     img: "images/Territorio 19.png",
-    text: "Información del territorio 4"
+    text: `
+    <div style="text-align: left;">
+      <strong>No visitar</strong><br><br>
+
+      Colón 982<br>
+
+      Nicaragua 263<br>
+    </div>
+  `
   },
 20: {
     img: "images/Territorio 20.png",
-    text: "Información del territorio 4"
+    text: `
+    <div style="text-align: left;">
+      <strong>No visitar</strong><br><br>
+    </div>
+  `
   },
 21: {
     img: "images/Territorio 21.png",
-    text: "Información del territorio 4"
+    text: `
+    <div style="text-align: left;">
+      <strong>No visitar</strong><br><br>
+    </div>
+  `
   },
 22: {
     img: "images/Territorio 22.png",
-    text: "Información del territorio 4"
+    text: `
+    <div style="text-align: left;">
+      <strong>No visitar</strong><br><br>
+    </div>
+  `
   },
 23: {
     img: "images/Territorio 23.png",
-    text: "Información del territorio 4"
+    text: `
+    <div style="text-align: left;">
+      <strong>No visitar</strong><br><br>
+    </div>
+  `
   },
 24: {
     img: "images/Territorio 24.png",
-    text: "Información del territorio 4"
+    text: `
+    <div style="text-align: left;">
+      <strong>No visitar</strong><br><br>
+
+      Seguí 74<br>
+      Febrero 2019<br>
+      
+    </div>
+  `
   },
 25: {
   img: "images/Territorio 25.png",
@@ -160,31 +262,62 @@ const cards = {
 
 26: {
     img: "images/Territorio 26.png",
-    text: "Información del territorio 4"
+    text: `
+    <div style="text-align: left;">
+      <strong>No visitar</strong><br><br>
+
+      Vidal 412<br>
+   ` 
+    </div>
+  `
   },
 27: {
     img: "images/Territorio 27.png",
-    text: "Información del territorio 4"
+    text: `
+    <div style="text-align: left;">
+      <strong>No visitar</strong><br><br>
+    </div>
+  `
   },
 28: {
     img: "images/Territorio 28.png",
-    text: "Información del territorio 4"
+    text: `
+    <div style="text-align: left;">
+      <strong>No visitar</strong><br><br>
+    </div>
+  `
   },
 29: {
     img: "images/Territorio 29.png",
-    text: "Información del territorio 4"
+    text: `
+    <div style="text-align: left;">
+      <strong>No visitar</strong><br><br>
+    </div>
+  `
   },
 30: {
     img: "images/Territorio 30.png",
-    text: "Información del territorio 4"
+    text: `
+    <div style="text-align: left;">
+      <strong>No visitar</strong><br><br>
+    </div>
+  `
   },
 31: {
     img: "images/Territorio 31.png",
-    text: "Información del territorio 4"
+    text: `
+    <div style="text-align: left;">
+      <strong>No visitar</strong><br><br>
+    </div>
+  `
   },
 32: {
     img: "images/Territorio 32.png",
-    text: "Información del territorio 4"
+    text: `
+    <div style="text-align: left;">
+      <strong>No visitar</strong><br><br>
+    </div>
+  `
   },
 33: {
   img: "images/Territorio 33.png",
@@ -203,99 +336,209 @@ const cards = {
 
 34: {
     img: "images/Territorio 34.png",
-    text: "Información del territorio 4"
+    text: `
+    <div style="text-align: left;">
+      <strong>No visitar</strong><br><br>
+
+      Lavalle 111<br>
+      Abril 2019<br>
+   ` 
   },
 35: {
     img: "images/Territorio 35.png",
-    text: "Información del territorio 4"
+    text: `
+    <div style="text-align: left;">
+      <strong>No visitar</strong><br><br>
+
+      Moreno 84<br>
+      27-07-2018<br>
+   ` 
   },
 36: {
     img: "images/Territorio 36.png",
-    text: "Información del territorio 4"
+    text: `
+    <div style="text-align: left;">
+      <strong>No visitar</strong><br><br>
+
+      25 de Mayo 187<br>
+      Diciembre 2015<br>
+   ` 
   },
 37: {
     img: "images/Territorio 37.png",
-    text: "Información del territorio 4"
+    text: `
+    <div style="text-align: left;">
+      <strong>No visitar</strong><br><br>
+    </div>
+  `
   },
 38: {
     img: "images/Territorio 38.png",
-    text: "Información del territorio 4"
+    text: `
+    <div style="text-align: left;">
+      <strong>No visitar</strong><br><br>
+
+      America 149-159 <br>
+      02-08-2018<br>
+
+      Ruta 210 29.221 <br>
+   ` 
   },
 39: {
     img: "images/Territorio 39.png",
-    text: "Información del territorio 4"
+    text: `
+    <div style="text-align: left;">
+      <strong>No visitar</strong><br><br>
+    </div>
+  `
   },
 40: {
     img: "images/Territorio 40.png",
-    text: "Información del territorio 4"
+    text: `
+    <div style="text-align: left;">
+      <strong>No visitar</strong><br><br>
+    </div>
+  `
   },
 41: {
     img: "images/Territorio 41.png",
-    text: "Información del territorio 4"
+    text: `
+    <div style="text-align: left;">
+      <strong>No visitar</strong><br><br>
+    </div>
+  `
   },
 42: {
     img: "images/Territorio 42.png",
-    text: "Información del territorio 4"
+    text: `
+    <div style="text-align: left;">
+      <strong>No visitar</strong><br><br>
+
+      Lorenzini 1346 <br>
+      24-09-2019<br>
+   ` 
   },
 43: {
     img: "images/Territorio 43.png",
-    text: "Información del territorio 4"
+    text: `
+    <div style="text-align: left;">
+      <strong>No visitar</strong><br><br>
+    </div>
+  `
   },
 44: {
     img: "images/Territorio 44.png",
-    text: "Información del territorio 4"
+    text: `
+    <div style="text-align: left;">
+      <strong>No visitar</strong><br><br>
+    </div>
+  `
   },
 45: {
     img: "images/Territorio 45.png",
-    text: "Información del territorio 4"
+    text: `
+    <div style="text-align: left;">
+      <strong>No visitar</strong><br><br>
+    </div>
+  `
   },
 46: {
     img: "images/Territorio 4.png",
-    text: "Información del territorio 4"
+    text: `
+    <div style="text-align: left;">
+      <strong>No visitar</strong><br><br>
+    </div>
+  `
   },
 47: {
   img: "images/Territorio 47.png",
-  text: "Información del territorio 47"
+  text: `
+    <div style="text-align: left;">
+      <strong>No visitar</strong><br><br>
+    </div>
+  `
 },
 48: {
   img: "images/Territorio 48.png",
-  text: "Información del territorio 48"
+  text: `
+    <div style="text-align: left;">
+      <strong>No visitar</strong><br><br>
+    </div>
+  `
 },
 49: {
   img: "images/Territorio 49.png",
-  text: "Información del territorio 49"
+  text: `
+    <div style="text-align: left;">
+      <strong>No visitar</strong><br><br>
+    </div>
+  `
 },
 50: {
   img: "images/Territorio 50.png",
-  text: "Información del territorio 50"
+  text: `
+    <div style="text-align: left;">
+      <strong>No visitar</strong><br><br>
+    </div>
+  `
 },
 51: {
   img: "images/Territorio 51.png",
-  text: "Información del territorio 51"
+  text: `
+    <div style="text-align: left;">
+      <strong>No visitar</strong><br><br>
+    </div>
+  `
 },
 52: {
   img: "images/Territorio 52.png",
-  text: "Información del territorio 52"
+  text: `
+    <div style="text-align: left;">
+      <strong>No visitar</strong><br><br>
+
+      Japón 2753 <br>
+      Marzo 2019<br>
+   ` 
 },
 53: {
   img: "images/Territorio 53.png",
-  text: "Información del territorio 53"
+  text: `
+    <div style="text-align: left;">
+      <strong>No visitar</strong><br><br>
+    </div>
+  `
 },
 54: {
   img: "images/Territorio 54 (RURAL).png",
-  text: "Información del territorio 54"
+  text: `
+    <div style="text-align: left;">
+      <strong>No visitar</strong><br><br>
+    </div>
+  `
 },
 55: {
   img: "images/Territorio 55 (RURAL).png",
-  text: "Información del territorio 55"
+  text: `
+    <div style="text-align: left;">
+      <strong>No visitar</strong><br><br>
+    </div>
+  `
 },
 56: {
   img: "images/Territorio 56 (RURAL).png",
-  text: "Información del territorio 56"
+  text: `
+    <div style="text-align: left;">
+      <strong>No visitar</strong><br><br>
+    </div>
+  `
 },
 57: {
   img: "images/Territorio 57 (RURAL).png",
-  text: "Información del territorio 57"
+  text: `
+    <div style="text-align: left;">
+      <strong>No visitar</strong><br><br>
+    </div>
+  `
 },
 
   // agregá más tarjetas acá
